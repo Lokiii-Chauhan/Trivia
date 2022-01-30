ae44feecde7090f376afdd5accf87e683c5aaaac
lokii <lokiiichauhan@gmail.com>
2022-01-31 00:17:57 +0530

    added high score and saved prefs

--- app/src/main/java/com/lokiiichauhan/trivia/MainActivity.java
+++ app/src/main/java/com/lokiiichauhan/trivia/MainActivity.java
@@ -20,31 +20,39 @@ import com.lokiiichauhan.trivia.data.AnswerListAsyncResponse;
 import com.lokiiichauhan.trivia.data.Repository;
 import com.lokiiichauhan.trivia.databinding.ActivityMainBinding;
 import com.lokiiichauhan.trivia.model.Question;
+import com.lokiiichauhan.trivia.model.Score;
+import com.lokiiichauhan.trivia.util.Prefs;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private int currentQuestionIndex = 0;
     private List<Question> questionList;
+    private int scoreCounter = 0;
+    private Score score;
+    private Prefs prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        score = new Score();
+        prefs = new Prefs(this);
 
+        currentQuestionIndex = prefs.getState();
+
+        binding.highestScoreTextview.setText("Highest Score = " + prefs.getHighestScore());
         questionList = new Repository().getQuestion(questionArrayList -> {
             binding.questionTextView.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
             updateCounter(questionArrayList);
         });
 
         binding.buttonNext.setOnClickListener(view -> {
-            currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
-            updateQuestion();
+            getNextQuestion();
         });
 
         binding.buttonTrue.setOnClickListener(view -> {
@@ -58,31 +66,56 @@ public class MainActivity extends AppCompatActivity{
         });
     }
 
+    private void getNextQuestion() {
+        currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+        updateQuestion();
+    }
+
     private void checkAnswer(boolean userChoseCorrect) {
         boolean answer = questionList.get(currentQuestionIndex).isAnswerTrue();
         int snackMessage = 0;
-        if (userChoseCorrect == answer){
+        if (userChoseCorrect == answer) {
             snackMessage = R.string.correct_answer;
+            addPoints();
             fadeAnimation();
-        }else {
+        } else {
             snackMessage = R.string.incorrect;
+            deductPoints();
             shakeAnimation();
         }
-        Snackbar.make(binding.cardView,snackMessage,Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(binding.cardView, snackMessage, Snackbar.LENGTH_SHORT).show();
     }
 
     private void updateCounter(ArrayList<Question> questionArrayList) {
         binding.textViewOutOf.setText(String.format(getString(R.string.text_formatted), currentQuestionIndex, questionArrayList.size()));
     }
 
-    private void updateQuestion(){
+    private void updateQuestion() {
         String question = questionList.get(currentQuestionIndex).getAnswer();
         binding.questionTextView.setText(question);
         updateCounter((ArrayList<Question>) questionList);
     }
 
-    private void shakeAnimation(){
-        Animation shake = AnimationUtils.loadAnimation(this,R.anim.shake_animation);
+    private void addPoints() {
+        scoreCounter += 100;
+        score.setScore(scoreCounter);
+        binding.scoreTextview.setText(String.valueOf("Score = " + score.getScore()));
+    }
+
+    private void deductPoints() {
+        if (scoreCounter > 0) {
+            scoreCounter -= 100;
+            score.setScore(scoreCounter);
+            binding.scoreTextview.setText(String.valueOf("Score = " + score.getScore()));
+        }else {
+            scoreCounter = 0;
+            score.setScore(scoreCounter);
+            binding.scoreTextview.setText(String.valueOf("Score = "+ score.getScore()));
+        }
+    }
+
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake_animation);
         binding.cardView.setAnimation(shake);
         shake.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -93,6 +126,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onAnimationEnd(Animation animation) {
                 binding.questionTextView.setTextColor(Color.WHITE);
+                getNextQuestion();
             }
 
             @Override
@@ -102,8 +136,8 @@ public class MainActivity extends AppCompatActivity{
         });
     }
 
-    private void fadeAnimation(){
-        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f,0.0f);
+    private void fadeAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
         alphaAnimation.setDuration(300);
         alphaAnimation.setRepeatMode(Animation.REVERSE);
         alphaAnimation.setRepeatMode(1);
@@ -118,6 +152,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onAnimationEnd(Animation animation) {
                 binding.questionTextView.setTextColor(Color.WHITE);
+                getNextQuestion();
             }
 
             @Override
@@ -125,5 +160,12 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        prefs.saveHighestScore(score.getScore());
+        prefs.setstate(currentQuestionIndex);
+        super.onPause();
     }
 }
---
+++ app/src/main/java/com/lokiiichauhan/trivia/util/Prefs.java
@@ -0,0 +1,39 @@
+package com.lokiiichauhan.trivia.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Prefs {
+    public static final String HIGHEST_SCORE = "highest_score";
+    public static final String STATE = "trivia_state";
+    private SharedPreferences preferences;
+
+    public Prefs(Activity context) {
+        this.preferences = context.getPreferences(Context.MODE_PRIVATE);
+
+
+    }
+
+    public void saveHighestScore(int score){
+        int currentScore = score;
+        int lastScore = preferences.getInt(HIGHEST_SCORE, 0);
+        if (currentScore > lastScore){
+            preferences.edit().putInt(HIGHEST_SCORE, currentScore).apply();
+        }
+
+    }
+
+    public int getHighestScore(){
+        return preferences.getInt(HIGHEST_SCORE,0);
+    }
+
+    public void setstate(int index){
+     preferences.edit().putInt(STATE, index).apply();
+    }
+
+    public int getState(){
+        return preferences.getInt(STATE, 0);
+    }
+
+}
